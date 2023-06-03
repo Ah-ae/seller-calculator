69855b8176a1d7c215a33e2cba0c785180594ce2
Ahae KIM <kimaae@gmail.com>
2023-06-03 14:25:21 +0900

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      white: "#FFF",
+      black: "#000",
+      gray100: "#F1F3F6",
+      gray200: "#CACFD9",
+      gray300: "#A1A9B4",
+      gray400: "#788290",
+      gray500: "#3D4755",
+      gray600: "#272E3A",
+      blue100: "#E3F0FF",
+      blue200: "#90C2FF",
+      blue300: "#377CFB",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
