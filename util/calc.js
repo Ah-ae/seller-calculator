11096e46d7841f9379e2fe4907bc4a647f1f96e9
Ahae KIM <kimaae@gmail.com>
2023-06-07 12:52:51 +0900

    마진율 계산 공식 변경

--- util/calc.js
+++ util/calc.js
@@ -25,12 +25,18 @@ export const calculateMarginRate = (
   purchaseCost,
   shipping,
   others,
+  commissionPrice,
   sellingPrice
 ) => {
   if (!purchaseCost || !shipping || !others || !sellingPrice) return;
 
+  // return (
+  //   (1 - (purchaseCost + shipping + others) / sellingPrice) *
+  //   100
+  // ).toFixed(2);
   return (
-    (1 - (purchaseCost + shipping + others) / sellingPrice) *
+    ((sellingPrice - purchaseCost - shipping - others - commissionPrice) /
+      sellingPrice) *
     100
   ).toFixed(2);
 };
--- util/formula.js
+++ util/formula.js
@@ -1,6 +1,7 @@
 export const formula = {
-  commissionAmount: "판매가 x 마켓수수료(%) * 0.01",
+  commissionAmount: "판매가 x 마켓수수료(%) x 0.01",
   settlementAmount: "판매가 - 배송비 - 마켓수수료(원)",
   marginPrice: "정산 금액 - 매입가 - 기타 비용",
-  marginRate: "{1 - (매입가 + 배송비 + 기타 비용) / 판매가} x 100",
+  marginRate:
+    "((판매가 - 매입가 - 배송비 - 기타비용 - 마켓수수료(원)) / 판매가) × 100",
 };
