a3312202786a65661a75222b303847bdfbe5e944
Ahae KIM <kimaae@gmail.com>
2023-06-07 12:52:35 +0900

    theme color 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,6 @@
 module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
-    tahiti: {
-      100: "#cffafe",
-      200: "#a5f3fc",
-      300: "#67e8f9",
-      400: "#22d3ee",
-      500: "#06b6d4",
-      600: "#0891b2",
-      700: "#0e7490",
-      800: "#155e75",
-      900: "#164e63",
-    },
-
     colors: {
       white: "#FFF",
       black: "#000",
