027a2c87f4632cd079e5cb624b8264784ae659fb
Ahae KIM <kimaae@gmail.com>
2023-06-09 15:43:09 +0900

    pwa 적용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+const config = {
+  reactStrictMode: true,
+};
+
+// * 캐시를 하려면 다음의 코드 추가
+// const runtimeCaching = require("next-pwa/cache.js");
+
+const nextConfig = withPWA({
+  dest: "public",
+  disable: !isProduction,
+  runtimeCaching: [],
+  // runtimeCaching,
+})(config);
+
+module.exports = nextConfig;
