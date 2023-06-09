bcb2eb2df5e03bd8946b4b9912c8603f2ebfabf7
Ahae KIM <kimaae@gmail.com>
2023-06-09 16:48:53 +0900

    og 관련 meta 태그 추가

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,31 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta
+          name="keywords"
+          content="온라인, 셀러, 계산기, 마진, 마진율, 판매가, 쇼핑몰, MD"
+        />
+        <meta
+          name="description"
+          content="마진, 판매가 등을 계산할 수 있는 웹 앱 계산기"
+        />
+        <meta name="author" content="Ahae Kim" />
+        <meta property="og:title" content="온라인 셀러 계산기" />
+        <meta
+          property="og:url"
+          content="https://https://seller-calculator.vercel.app"
+        />
+        <meta property="og:site_name" content="셀러 계산기" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="마진, 판매가 등을 계산할 수 있는 웹 앱 계산기"
+        />
+        <meta
+          property="og:image"
+          content="https://https://seller-calculator.vercel.app/logo.svg"
+        />
+        <meta property="og:local" content="ko-KR" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
