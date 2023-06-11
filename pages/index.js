f7d960240c25ad11cf29a6f23a4b2c9e65e99ce3
Ahae KIM <kimaae@gmail.com>
2023-06-11 13:33:56 +0900

    index 페이지 <Head> 컴포넌트 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
-import Head from "next/head";
 import Margin from "./margin";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>온라인 셀러 계산기</title>
-      </Head>
-      <Margin />
-    </>
-  );
+  return <Margin />;
 }
