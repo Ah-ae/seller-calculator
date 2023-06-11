92b10242ab459f5fb5890631efd0c23cfbb06fcb
Ahae KIM <kimaae@gmail.com>
2023-06-11 13:32:22 +0900

    <title> 태그 수정

--- components/Seo.js
+++ components/Seo.js
@@ -1,16 +1,12 @@
 import Head from "next/head";
 
-export default function Seo({ title, style }) {
+export default function Seo({ title }) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{`온라인 셀러 계산기 | ${title}`}</title>
       </Head>
-      <h2
-        className={
-          "pt-6 pb-4 text-4xl text-gray-500 max-md:text-3xl max-md:text-center"
-        }
-      >
+      <h2 className="pt-6 pb-4 text-4xl text-gray-500 max-md:text-3xl max-md:text-center">
         {title}
       </h2>
     </>
