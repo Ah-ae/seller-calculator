e1f76c5c06a1d14fa98c6d39f8d2d6cf1456dd5b
Ahae KIM <kimaae@gmail.com>
2023-06-12 14:46:43 +0900

    service description 수정

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,11 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           name="keywords"
-          content="온라인, 셀러, 계산기, 마진, 마진율, 판매가, 쇼핑몰, MD"
+          content="온라인, 셀러, 계산기, 마진, 마진율, 판매가, 할인율, 행사가, 행사가 환원, 쇼핑몰, MD"
         />
         <meta
           name="description"
-          content="마진, 판매가 등을 계산할 수 있는 웹 앱 계산기"
+          content="마진, 판매가, 행사가 환원 등을 계산할 수 있는 웹 앱 계산기"
         />
         <meta name="author" content="Ahae Kim" />
         <meta property="og:title" content="온라인 셀러 계산기" />
@@ -23,7 +23,7 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta
           property="og:description"
-          content="마진, 판매가 등을 계산할 수 있는 웹 앱 계산기"
+          content="마진, 판매가, 행사가 환원 등을 계산할 수 있는 웹 앱 계산기"
         />
         <meta
           property="og:image"
