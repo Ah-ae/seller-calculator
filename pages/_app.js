485039c6fbe1383bc4b8caecdbe20d3c0ea5d67f
Ahae KIM <kimaae@gmail.com>
2023-06-27 11:47:38 +0900

    gtag 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
+import Script from "next/script";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/*  Google tag (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      ></Script>
+      <Script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${gtag.GA_TRACKING_ID}');
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
